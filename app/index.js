9a53fdc50f3d078f0e9f93deb33a0f9506263823
BKhurshid <josephsun4@gmail.com>
2016-03-21 20:35:42 -0700

    Added variable inside the html in the return statement. Deleted one argument and the console.log

--- app/index.js
+++ app/index.js
@@ -4,13 +4,12 @@ var ReactDOM = require('react-dom');
 var HelloWorld = React.createClass({
     render: function() {
         return(
-               console.log("This.props", this.props);
-               <div> Hello Reactjs Tutorial </div>
+               <div> Hello {this.props.name} </div>
                );
     }
 });
 
 ReactDOM.render(
-    < HelloWorld name= "Tyler" anySortOfData = {29}/>,
+    < HelloWorld name= "Tyler" />,
     document.getElementById('app')
 );
