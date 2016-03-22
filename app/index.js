db93ae88ddc06c779b0c608fb1138082ee62f8bb
BKhurshid <josephsun4@gmail.com>
2016-03-21 20:40:02 -0700

    Added object with data. Deleted helloWorld Componente

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-    render: function() {
-        return(
-               <div> Hello {this.props.name} </div>
-               );
-    }
-});
+
+var USER_DATA = {
+    name: 'Tyler McGinnis',
+    username: 'tylermcginnis', 
+    image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+}
+var 
 
 ReactDOM.render(
     < HelloWorld name= "Tyler" />,
