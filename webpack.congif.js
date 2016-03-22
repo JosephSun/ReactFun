7c9a9c17176cce72df8dd91821e0574c9657600e
BKhurshid <josephsun4@gmail.com>
2016-03-21 17:24:25 -0700

    added a small space

--- webpack.congif.js
+++ webpack.congif.js
@@ -6,7 +6,9 @@ module.exports = {
         loaders: [
             {test:/\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
         ]
+    },
+    output: {
+        filename: "index_bundle.js",
+        path: __dirname + '/dist'
     }
 }; 
-
-
