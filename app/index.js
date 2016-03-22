28f2de72a99c09ac31f741b3b09ac401dcf109ea
BKhurshid <josephsun4@gmail.com>
2016-03-21 20:43:23 -0700

    Added a profileLink component

--- app/index.js
+++ app/index.js
@@ -7,7 +7,23 @@ var USER_DATA = {
     username: 'tylermcginnis', 
     image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
 }
-var 
+var ProfilePic = React.createClass({
+    render: function() {
+        return <img src = {this.props.imageUrl} style = {{height: 100, width: 100}} />
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <a href= {'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        )
+    }
+})
 
 ReactDOM.render(
     < HelloWorld name= "Tyler" />,
