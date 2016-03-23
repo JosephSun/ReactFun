4931c6c53591e67592eb71098136d2c0fafda65d
BKhurshid <josephsun4@gmail.com>
2016-03-22 19:38:04 -0700

    Changed up my code with his code a little bit but its seems exactly the same in the important ways. Odd. I don't know what was causing my code to bug out.

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,15 +3,24 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
-var routes = {
-    <Router>
-        <Route path = '/' component={Main}>
-            <Route path ='/home' component={Home} />
-        </Route>
-    </Router>
-}
+// var routes = {
+// <Router>
+//         <Route path = '/' component={Main}>
+//             <Route path ='/home' component={Home} />
+//         </Route>
+//     </Router>
+// };
 
+var routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>
+);
 module.exports = routes;
+
