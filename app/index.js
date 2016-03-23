d52b857d5cccb723ee65db0f21a36dcf104bd1a4
BKhurshid <josephsun4@gmail.com>
2016-03-22 18:20:42 -0700

    fixed error, missing = sign in the Link component, span tag, between style and curly brackets

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ var ProfilePic = React.createClass({
 var Link = React.createClass({
     render: function() {
         return (
-                <span style {{color: 'blue', cursor: 'pointer'}}> 
+                <span style ={{color: 'blue', cursor: 'pointer'}}> 
                     {this.props.children}
                 </span>
                 );
