5ba9a70caba7f88153da7d18f823e2c41be4362c
BKhurshid <josephsun4@gmail.com>
2016-03-23 18:09:09 -0700

    Building out header for component

--- app/containers/promptContainer.js
+++ app/containers/promptContainer.js
@@ -2,7 +2,8 @@ var React = require('react');
 
 var PromptContainer = React.createClass ({
     render: function() {
-
+        <div className = "jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <h1>Some Header Text</h1>
     }
 });
 module.exports = PromptContainer;
