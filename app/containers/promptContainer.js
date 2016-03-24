5feaa575c3643c333faf15e9750f1b3cd32f44ba
BKhurshid <josephsun4@gmail.com>
2016-03-23 18:16:06 -0700

    Finished prompt container

--- app/containers/promptContainer.js
+++ app/containers/promptContainer.js
@@ -13,8 +13,18 @@ var PromptContainer = React.createClass ({
                             type="text" />
 
                 </div>
+                 <div className= "form-group col-sm-4 col-sm-offset-4">
+                    <button 
+                        className="btn btn-block btn-success"
+                        type="submit">
+                        Continue
+                    </buttton> 
+                </div>
+            </form>
             </div>
         </div>
     }
 });
+
+
 module.exports = PromptContainer;
