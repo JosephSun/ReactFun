b27fadbb66fb2451f843832bb154553fbe658385
BKhurshid <josephsun4@gmail.com>
2016-03-23 18:12:32 -0700

    Added a input box

--- app/containers/promptContainer.js
+++ app/containers/promptContainer.js
@@ -3,7 +3,18 @@ var React = require('react');
 var PromptContainer = React.createClass ({
     render: function() {
         <div className = "jumbotron col-sm-6 col-sm-offset-3 text-center">
-        <h1>Some Header Text</h1>
+            <h1>Some Header Text</h1>
+            <div classNam="col-sm-12">
+                <form>
+                    <div className="form-group">
+                        <input
+                            className="form-control"
+                            placeholder="Github Username"
+                            type="text" />
+
+                </div>
+            </div>
+        </div>
     }
 });
 module.exports = PromptContainer;
