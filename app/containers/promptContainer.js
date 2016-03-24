cab0ad72899af94670efbc73369a324922d5b978
BKhurshid <josephsun4@gmail.com>
2016-03-23 19:47:38 -0700

    Added the tutorials source code b/c cannot find promptContainer module

--- app/containers/promptContainer.js
+++ app/containers/promptContainer.js
@@ -1,30 +1,49 @@
 var React = require('react');
+var Prompt = require('../components/Prompt');
 
-var PromptContainer = React.createClass ({
-    render: function() {
-        <div className = "jumbotron col-sm-6 col-sm-offset-3 text-center" >
-            <h1>Some Header Text</h1>
-            <div classNam="col-sm-12">
-                <form>
-                    <div className="form-group">
-                        <input
-                            className="form-control"
-                            placeholder="Github Username"
-                            type="text" />
-
-                </div>
-                 <div className= "form-group col-sm-4 col-sm-offset-4">
-                    <button
-                        className="btn btn-block btn-success"
-                        type="submit">
-                        Continue
-                    </buttton>
-                </div>
-            </form>
-        </div>
-    </div>
+var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      username: ''
     }
-});
+  },
+  handleSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
 
+    if (this.props.routeParams.playerOne) {
+      var pathname =
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username,
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+  handleUpdateUser: function (event) {
+    this.setState({
+      username: event.target.value
+    });
+  },
+  render: function () {
+    return (
+      <Prompt
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
+    )
+  }
+});
 
 module.exports = PromptContainer;
