a6b9cb9a5bce4fd85026ac3c39730e1c77dc3ed5
BKhurshid <josephsun4@gmail.com>
2016-03-24 22:21:39 -0700

    added tutorial code to see problem

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,13 +1,14 @@
+
 var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
+var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
-var Home = require('../components/Home');
-var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer.js');
+var Home = require("../components/Home");
 var PromptContainer = require('../containers/PromptContainer');
+var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 
 var routes = (
   <Router history={hashHistory}>
@@ -15,7 +16,7 @@ var routes = (
       <IndexRoute component={Home} />
       <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
-      <Route path='battle' header='Player Two' component={ConfirmBattleContainer} />
+      <Route path='battle' component={ConfirmBattleContainer} />
     </Route>
   </Router>
 );
