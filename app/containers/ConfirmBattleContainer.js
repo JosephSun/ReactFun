7c3c69ed68e1af38dcb187381558aeb4b2f289e5
BKhurshid <josephsun4@gmail.com>
2016-03-24 21:50:00 -0700

    added confirmBattleContainer module and context types

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,9 +1,12 @@
 var React = require('react');
-
+var ConfirmBattle = require('../components/ConfirmBattle');
 var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  }
   render: function() {
     return (
-      <div/>
+      <ConfirmBattle/>
     )
   }
 })
