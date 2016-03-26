5d44f9db7d1eff6da67305c356384693888fd613
BKhurshid <josephsun4@gmail.com>
2016-03-25 18:15:36 -0700

    defined function to getUserInfo

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -3,3 +3,7 @@ const axios = require('axios');
 const id = "YOUR_CLIENT_ID";
 const id = "YOUR_SECRET_ID";
 const id = "?client_id=" + id + "&client_secret=" + sec;
+
+function getUserInfo(username) {
+  return axios.get("https://api.github.com/users/" + username + param);
+}
